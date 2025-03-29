1ad7f4619c9d68108de54aa36a76ea4c9c7509a1
Василенко Илья <vasilenko.iv.92@yandex.ru>
2025-03-29 11:02:29 +0300

    feat: homework2:2

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -10,36 +10,34 @@ resource "yandex_vpc_subnet" "develop" {
 
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2404-lts"
+  family = var.vm_web_image_os_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v3"
-  // TODO: опечатка в значении ключа platform_id. Исправленно значение standarT-v*
+  name        = var.vm_web_platform_setting.vm_name
+  platform_id = var.vm_web_platform_setting.platform_id
+
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 20
-    //TODO: Не допустимые значения cores = 2; core_fraction =5.
-    // При указании cores 5 будет выделенно 5 ядер, что увеличит производительность
-    // и позволит выполнять больще процессов параллельно.
+    cores         = var.vm_web_platform_setting.resources.cores
+    memory        = var.vm_web_platform_setting.resources.memory
+    core_fraction = var.vm_web_platform_setting.resources.core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
   scheduling_policy {
-    preemptible = true
-    //TODO: параметр отвечающий за свойство перрываимости у ВМ
+    preemptible = var.vm_web_platform_setting.scheduling_policy.preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_platform_setting.network_interface.is_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_platform_setting.metadata.serial_port_enable
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
 
--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -36,3 +36,54 @@ variable "vms_ssh_root_key" {
   default     = "заглушка ssh ключа"
   description = "ssh-keygen -t ed25519"
 }
+
+variable "vm_web_image_os_family" {
+  type        = string
+  default     = "ubuntu-2404-lts"
+  description = "Уникальный идентификатор семейста ОС для образа ВМ"
+}
+
+variable "vm_web_platform_setting" {
+    type = object({
+      vm_name        = string
+      platform_id = string
+      resources   = object({
+        cores         = number
+        memory        = number
+        core_fraction = number
+      })
+
+      scheduling_policy = object({
+        preemptible = bool
+      })
+      network_interface = object({
+        is_nat       = bool
+      })
+      metadata = object({
+        serial_port_enable = number
+      })
+    })
+    ### Значения по умолчанию
+    default = {
+      vm_name     = "netology-develop-platform-web"
+      platform_id = "standard-v3"
+      resources = {
+        cores         = 2
+        memory        = 2
+        core_fraction = 20
+      }
+
+      scheduling_policy = {
+        preemptible = true
+      }
+      network_interface = {
+        is_nat       = true
+      }
+      metadata = {
+        serial_port_enable = 1
+      }
+    }
+    description = "Для конфигурирования платформы!!!"
+  }
+
+
