f10e5afa57c4a4917c1aaef2c80fd571f5eebc78
Василенко Илья <vasilenko.iv.92@yandex.ru>
2025-04-04 09:06:36 +0300

    fix: homework3:4

--- 03/src/inventory.tf
+++ 03/src/inventory.tf
@@ -1,25 +1,27 @@
 resource "local_file" "hosts_templatefile" {
   content = templatefile("${path.module}/hosts.tftpl", {
-    webservers = [
-    for vm in yandex_compute_instance.virtual_machine : {
-      name               = vm.name
-      nat_ip_address     = vm.network_interface[0].nat_ip_address
-      fqdn               = vm.metadata["fqdn"]
-    }
+    webservers = flatten([
+    for vm in yandex_compute_instance.virtual_machine : [
+      {
+        name               = vm.name
+        nat_ip_address     = vm.network_interface[0].nat_ip_address
+        fqdn               = vm.metadata["fqdn"]
+      }
     ]
+    ])
     databases = [
     for vm in yandex_compute_instance.virtual_db : {
-      name               = vm.name
-      nat_ip_address     = vm.network_interface[0].nat_ip_address
-      fqdn               = vm.metadata["fqdn"]
+      name           = vm.name
+      nat_ip_address = vm.network_interface[0].nat_ip_address
+      fqdn           = vm.metadata["fqdn"]
     }
     ]
     storage = [
-    for vm in [yandex_compute_instance.vm_singleton] : {
-        name               = yandex_compute_instance.vm_singleton.name
-        nat_ip_address     = yandex_compute_instance.vm_singleton.network_interface[0].nat_ip_address
-        fqdn               = yandex_compute_instance.vm_singleton.metadata["fqdn"]
-      }
+    for vm in flatten([yandex_compute_instance.vm_singleton]) : {
+      name               = vm.name
+      nat_ip_address     = vm.network_interface[0].nat_ip_address
+      fqdn               = vm.metadata["fqdn"]
+    }
     ]
     line_separator = "\n"
   })
