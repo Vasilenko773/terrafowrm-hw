8b295a406816e76a42c05774d0dcac9041d398a1
Василенко Илья <vasilenko.iv.92@yandex.ru>
2025-04-06 11:16:44 +0300

    feat: homework4:1

--- 04/demonstration1/vms/variables.tf
+++ 04/demonstration1/vms/variables.tf
@@ -1,6 +1,16 @@
 ###cloud vars
 
-variable "public_key" {
-  type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiVcfW8Wa/DxbBNzmQcwn7hJOj7ji9eoTpFakVnY/AI webinar"
+variable "path_to_public_key" {
+  type        = string
+  description = "stub private key"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
